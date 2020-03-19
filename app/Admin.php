61baec3c13c70563aceba6681e5e3457d5a2f6c8
winstor <540124900@qq.com>
2020-03-19 12:24:07 +0800

    添加管理员表以及迁移文件，安装权限插件

---
+++ app/Admin.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Traits\HasRoles;
+
+class Admin extends Model
+{
+    use HasRoles;
+    //
+}
---
+++ database/migrations/2020_03_19_041822_create_admins_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('username',50)->unique()->comment('账号');
+            $table->string('email',100)->nullable()->unique()->comment('邮箱');
+            $table->string('password',50)->comment('密码');
+            $table->string('name',30)->nullable()->comment('姓名');
+            $table->string('phone',20)->nullable()->comment('电话');
+            $table->string('avatar')->nullable()->comment('头像');
+            $table->timestamp('last_login')->nullable()->comment('最后登录时间');
+            $table->string('last_login_ip',30)->nullable()->comment('最后登录IP');
+            $table->tinyInteger('enabled')->default(1)->comment('是否可用');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+}
